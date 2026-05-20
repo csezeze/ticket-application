3b1defbbc84638c802bbfde4027c7aa672fd9fb4
csezeze <csezeze@gmail.com>
2026-05-20 22:15:44 +0300

    Add basic home screen

--- app/src/main/java/com/turkcell/ticketapp/navigation/AppNavHost.kt
+++ app/src/main/java/com/turkcell/ticketapp/navigation/AppNavHost.kt
@@ -1,18 +1,14 @@
 package com.turkcell.ticketapp.navigation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.turkcell.ticketapp.screen.LoginScreen
 import com.turkcell.ticketapp.screen.RegisterScreen
-
+import com.turkcell.ticketapp.screen.HomeScreen
 @Composable
 fun AppNavHost(
     navController: NavHostController = rememberNavController()
@@ -45,12 +41,7 @@ fun AppNavHost(
         }
 
         composable<Home> {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text("Giriş başarılı. Ana sayfaya hoş geldiniz.")
-            }
+            HomeScreen()
         }
     }
 }
---
+++ app/src/main/java/com/turkcell/ticketapp/screen/HomeScreen.kt
@@ -0,0 +1,33 @@
+package com.turkcell.ticketapp.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Ana Sayfa Test",
+                style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    }
+}
