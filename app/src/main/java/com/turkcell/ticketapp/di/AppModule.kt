9ef23a2fb7c2e067955e89e5e04b99b800e06d0a
csezeze <csezeze@gmail.com>
2026-05-22 13:06:56 +0300

    Add home view model

--- app/src/main/java/com/turkcell/ticketapp/di/AppModule.kt
+++ app/src/main/java/com/turkcell/ticketapp/di/AppModule.kt
@@ -4,7 +4,7 @@ import com.turkcell.ticketapp.viewmodel.LoginViewModel
 import com.turkcell.ticketapp.viewmodel.RegisterViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
-
+import com.turkcell.ticketapp.viewmodel.HomeViewModel
 val appModule = module {
     viewModelOf(::LoginViewModel)
     viewModelOf(::RegisterViewModel)
